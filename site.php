553a31ebb6139d826a19a76587df352f931f7f27
mdajgarhossain <mdajgarhossain1@gmail.com>
2021-03-30 16:42:03 +0600

    Declare & use variables

--- site.php
+++ site.php
@@ -7,10 +7,26 @@
   <body>
 
     <?php
-      echo "Hello World!";
-      echo "<p>This is my website over the internet.</p>";
-      echo "<h2>Learn PHP fundamentals</h2>";
-      echo "<hr />";
+      echo "There once was a man named <b>Navin Thapa</b>. <br>";
+      echo "He was 30 years old. <br>";
+      echo "He really liked the name Navin. <br>";
+      echo "But didn't like being 30. <br>";
+      echo "<hr/>";
+      // Another way
+      echo "There once was a man named <b>Navin Thapa</b>. <br>
+      He was 30 years old. <br>
+      He really liked the name Navin. <br>
+      But didn't like being 30. <br>
+      <hr/>";
+      // Another way using variables
+      $characterName = "Navin Thapa";
+      $characterAge = 30;
+      echo "There once was a man named <b>$characterName</b>. <br>";
+      echo "He was $characterAge years old. <br>";
+      $characterName = "Karim Uthapa";
+      echo "He really liked the name $characterName. <br>";
+      echo "But didn't like being $characterAge. <br>";
+      echo "<hr/>";
      ?>
   </body>
 </html>
