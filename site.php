1d3434fd836683ea3fd9716b2e461f496935501f
mdajgarhossain <mdajgarhossain1@gmail.com>
2021-03-30 12:34:54 +0600

    Setup php & writing html php

---
+++ site.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Fundamentals</title>
+  </head>
+  <body>
+
+    <?php
+      echo "Hello World!";
+      echo "<p>This is my website over the internet.</p>";
+      echo "<h2>Learn PHP fundamentals</h2>";
+      echo "<hr />";
+     ?>
+  </body>
+</html>
